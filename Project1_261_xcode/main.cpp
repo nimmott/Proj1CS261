9c4a8cc0aa0846d50c4d24e2b95763db4e1d9667
Harrison Brace <hbrace@gmail.com>
2015-09-15 14:46:16 -0500

    Initial Commit

---
+++ Project1_261_xcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project1_261_xcode
+//
+//  Created by Harrsion Brace on 9/15/15.
+//  Copyright (c) 2015 HarrisonBrace. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
